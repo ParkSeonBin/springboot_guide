67646093022d87c9a6e145b8e0a51e498322bb81
ParkSeonBin <95379549+ParkSeonBin@users.noreply.github.com>
2023-10-19 14:10:29 +0900

    Delete src/main/java/com/springboot/valid_exception/data/repository/ProducerRepository.java

--- src/main/java/com/springboot/valid_exception/data/repository/ProducerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.valid_exception.data.repository;
-
-import com.springboot.valid_exception.data.entity.Producer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProducerRepository extends JpaRepository<Producer, Long> {
-}
