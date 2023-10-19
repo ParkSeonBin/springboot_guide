25ddc02c46c0775f407820fd7c420cc2e7b61463
ParkSeonBin <95379549+ParkSeonBin@users.noreply.github.com>
2023-10-19 14:09:32 +0900

    Delete src/main/java/com/springboot/valid_exception/data/repository/ProductDetailRepository.java

--- src/main/java/com/springboot/valid_exception/data/repository/ProductDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.valid_exception.data.repository;
-
-import com.springboot.valid_exception.data.entity.ProductDetail;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductDetailRepository extends JpaRepository<ProductDetail, Long> {
-}
