e4306b07e6b9c3786dc3ef6f66d710d126a19451
ParkSeonBin <95379549+ParkSeonBin@users.noreply.github.com>
2023-10-19 14:10:39 +0900

    Delete src/main/java/com/springboot/valid_exception/data/repository/ProviderRepository.java

--- src/main/java/com/springboot/valid_exception/data/repository/ProviderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.valid_exception.data.repository;
-
-import com.springboot.valid_exception.data.entity.Provider;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProviderRepository extends JpaRepository<Provider, Long> {
-}
